40a8ae0f1069649b761f69574f8b0f909072c0ad
Khuai <khaichet1708@gmail.com>
2026-01-16 11:50:35 +0700

    Refactor Home component: simplify navigation by removing Link and using redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
 import { Camera } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <Link href="/camera">
-        <button className="flex items-center gap-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-          <Camera size={20} />
-          Camera
-        </button>
-      </Link>
-    </div>
-  );
+  redirect("/camera");
 }
